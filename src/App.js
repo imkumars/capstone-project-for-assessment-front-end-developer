3c9033d651cffc4d0d27eafcc971167a8ea61719
imkumars <kumarsgj1@gmail.com>
2023-12-13 02:53:39 +0530

    Add Semantic HTML tags, Meta tags and Open graph protocol

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 
 import './App.css';
+import Header from './Components/Header';
+import Nav from './Components/Nav';
+import Main from './Components/Main';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
-      Homepage
+
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+      
     </div>
   );
 }
